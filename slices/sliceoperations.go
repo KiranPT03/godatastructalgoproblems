6c42397f19039eb8d4e7426d6f5ffcff1104f00b
KiranPT03 <kiran.p.tavadare@live.com>
2021-01-27 22:04:02 +0530

    Update sliceoperations.go
    Added iterateOveSlice function to access element of slices

--- slices/sliceoperations.go
+++ slices/sliceoperations.go
@@ -1,5 +1,20 @@
-package slices
+package main
+
+import (
+	"fmt"
+)
+
+// iterateOverSlice is a fucntion to iterate given slice
+func iterateOverSlice(data []int) {
+	for i, val := range data {
+		fmt.Println(i, val)
+	}
+
+}
 
 func main() {
+	fmt.Println("Introduction to slice operations")
+	A := []int{1, 2, 3, 4, 5}
+	iterateOverSlice(A)
 
 }
